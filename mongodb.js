640cc8ba7675a8e6e0597be5580f0c5cfe8e31ed
Ian Finch <ianfinch@users.noreply.github.com>
2022-04-15 23:56:32 +0100

    Added mongoose connection to API response

---
+++ mongodb.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+const connectionString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`
+
+const mongoosePromise = mongoose.connect(connectionString, { useNewUrlParser: true });
+
+export default mongoosePromise;
--- public/api/customers.js
+++ public/api/customers.js
@@ -1,5 +1,11 @@
+import mongoosePromise from "../../lib/mongodb.js";
+
 export default function handler(request, response) {
-  response.status(200).json({
-    body: {a: "hello", b: "world"}
-  });
+  mongoosePromise
+    .then(mongoose => {
+        console.log(mongoose);
+        response.status(200).json({
+            body: {a: "hello", b: "world"}
+        });
+    });
 }
