be32a7b1053abb713c2d8eccd8b155c30f5d36e7
Ian Finch <ianfinch@users.noreply.github.com>
2022-04-19 12:23:36 +0100

    Seeing if we can add a third level to a REST API

---
+++ public/api/customers/[id]/offers.js
@@ -0,0 +1,7 @@
+const utils = require("../../../lib/handler-utils.js");
+
+export default function(request, response) {
+
+    Promise.resolve("TEST CALL")
+        .then(utils.returnResult(200, request, response));
+};
