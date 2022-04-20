c3b344d9f581b4fe74f9c8702adb78edc8fe9998
Ian Finch <ianfinch@users.noreply.github.com>
2022-04-20 08:59:40 +0100

    Started adding DB calls to offers.js

--- public/api/customers/[id]/offers.js
+++ public/api/customers/[id]/offers.js
@@ -1,7 +1,13 @@
 const utils = require("../../../../lib/handler-utils.js");
+const models = require("../../lib/models.js");
 
 export default function(request, response) {
 
-    Promise.resolve("TEST CALL")
-        .then(utils.returnResult(200, request, response));
+    const customerModel = models.person.model;
+    const productModel = models.product.model;
+
+    Promise.all([
+        customerModel.findById(request.query.id),
+        productModel.find()
+    ]).then(utils.returnResult(200, request, response));
 };
